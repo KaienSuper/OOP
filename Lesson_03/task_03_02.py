246726829ef33ef3e3aa96648eb57427f95e8702
KaienSuper <halilovaybulat@gmail.com>
2024-03-25 08:47:27 +0500

    complete

---
+++ Lesson_03/task_03_02.py
@@ -0,0 +1,15 @@
+class ReversedList:
+    def __init__(self, lst):
+        self.lst = lst
+ 
+    def __len__(self):
+        return len(self.lst)
+ 
+    def __getitem__(self,index):
+        return self.lst[-1-index]
+
+# Ваш код
+
+rl = ReversedList([10, 20, 30])
+for i in range(len(rl)):
+    print(rl[i])
